db9ed3010f6a8afd61155c73cf68c71dc0add1ac
CoolSpy3 <coolspythree@gmail.com>
2021-12-11 01:32:39 -0800

    update to version 1.0.2

--- src/main/java/com/coolspy3/util/CoolSpyLib.java
+++ src/main/java/com/coolspy3/util/CoolSpyLib.java
@@ -7,7 +7,7 @@ import com.coolspy3.csmodloader.network.SubscribeToPacketStream;
 import com.coolspy3.cspackets.packets.GameJoinPacket;
 
 
-@Mod(id = "csutils", name = "CSUtils", version = "1.0.1",
+@Mod(id = "csutils", name = "CSUtils", version = "1.0.2",
         description = "Adds utility functions for use by other mods",
         dependencies = {"csmodloader:[1.0.0,2)", "cspackets:[1.0.0,2]"})
 public class CoolSpyLib implements Entrypoint
